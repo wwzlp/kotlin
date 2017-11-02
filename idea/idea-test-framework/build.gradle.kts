ef6460d1397513d923e65731b66b77af8875c396
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-11-02 18:55:20 +0100

    Convert idea plugin modules to intellij plugin

--- idea/idea-test-framework/build.gradle.kts
+++ idea/idea-test-framework/build.gradle.kts
@@ -1,6 +1,8 @@
 
 apply { plugin("kotlin") }
 
+configureIntellijPlugin()
+
 dependencies {
     compile(project(":compiler:frontend"))
     compile(project(":compiler:frontend.script"))
@@ -11,7 +13,12 @@ dependencies {
     compile(project(":kotlin-test:kotlin-test-jvm"))
     compileOnly(project(":kotlin-reflect-api"))
     compile(commonDep("junit:junit"))
-    compile(ideaSdkDeps("openapi", "idea"))
+}
+
+afterEvaluate {
+    dependencies {
+        compile(intellij { include("openapi.jar", "idea.jar") })
+    }
 }
 
 sourceSets {
